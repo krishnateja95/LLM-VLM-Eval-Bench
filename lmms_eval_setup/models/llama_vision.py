e8c708797768eef25d5f5086af555c7855dcfbaf
Krishna Teja Chitty-Venkata <krishnateja95@outlook.com>
2024-10-17 02:14:12 +0000

    Updated lmms-eval

--- lmms_eval_setup/models/llama_vision.py
+++ lmms_eval_setup/models/llama_vision.py
@@ -209,8 +209,9 @@ class LlamaVision(lmms):
             for _ in range(len(images)):
                 messages[-1]["content"].append({"type": "image"})
             messages[-1]["content"].append({"type": "text", "content": contexts})
+            
             prompt = self.processor.apply_chat_template(messages, add_generation_prompt=True)
-            inputs = self.processor(images, prompt, return_tensors="pt").to(self.model.device)
+            inputs = self.processor(images = images, text = prompt, return_tensors="pt").to(self.model.device)
 
             if "max_new_tokens" not in gen_kwargs:
                 gen_kwargs["max_new_tokens"] = 1024
--- lmms_eval_setup/run.sh
+++ lmms_eval_setup/run.sh
@@ -7,9 +7,9 @@ conda activate lmms_eval
 export HF_HOME='/lus/grand/projects/datascience/krishnat/model_weights/LLaMA/llama_cache/'
 export HF_DATASETS_CACHE='/lus/grand/projects/datascience/krishnat/model_weights/LLaMA/llama_cache/'
 
-python3 -m accelerate.commands.launch --num_processes=1 main.py \
+python3 -m accelerate.commands.launch --num_processes=8 main.py \
     --model llama_vision \
-    --model_args pretrained="meta-llama/Llama-3.2-11B-Vision" \
+    --model_args pretrained="meta-llama/Llama-3.2-11B-Vision-Instruct" \
     --tasks mme,mmbench_en \
     --batch_size 1 \
 
